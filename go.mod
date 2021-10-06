2d978e22673b0ead5c308cd35477a9324552c233
Cole Bosmann <cole@netlify.com>
2021-10-06 10:28:27 -0700

    added resp status header

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cbosss/functions-playground
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.27.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.27.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
 )
 
 const failHeader = "x-nf-fail"
+const statusCodeHeader = "x-nf-status-code"
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	if request.Headers[failHeader] == "fail" {
 		return nil, errors.New("fail header detected")
 	}
 
+	status := http.StatusOK
+	if request.Headers[statusCodeHeader] != "" {
+		statusi, err := strconv.ParseInt(request.Headers[statusCodeHeader], 10, 64)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed processing ")
+		}
+		status = int(statusi)
+	}
+
 	return &events.APIGatewayProxyResponse{
-		StatusCode:      200,
+		StatusCode:      status,
 		Headers:         map[string]string{"content-type": "application/json"},
 		Body:            fmt.Sprintf(`{"timestamp": %s}`, time.Now()),
 		IsBase64Encoded: false,
