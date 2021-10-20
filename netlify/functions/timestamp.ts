363ab0bf7a3f7544c8f61f2d66faabd350c8a78d
Cole Bosmann <cole@netlify.com>
2021-10-20 11:57:59 -0700

    throwing in JS

--- netlify/functions/timestamp.ts
+++ netlify/functions/timestamp.ts
@@ -1,6 +1,7 @@
 import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
+  throw("synthetic error")
   return {
     statusCode: 50,
     body: JSON.stringify({
